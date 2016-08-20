f234e826ec1e05e413567f2d4ec3955ffbc6709c
masiv <masiv@hismith.ru>
2016-08-20 15:57:41 +0400

    Add to get Redis connection method support. For using this in Redis modules like symfony redis session handler

--- Client.php
+++ Client.php
@@ -338,6 +338,15 @@ class Credis_Client {
     }
 
     /**
+     * Return the \Redis instance
+     * @return \Redis
+     */
+    public function getRedis()
+    {
+        return $this->redis;
+    }
+
+    /**
      * Return the selected database
      * @return int
      */
